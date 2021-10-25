36c0a098b6f14cb657c91098e47b585d2db0222e
Davit Barblishvili <db3230@columbia.edu>
2021-10-24 20:55:16 -0400

    completed part 1 and 2

--- hw2/cky.py
+++ hw2/cky.py
@@ -1,6 +1,6 @@
 """
 COMS W4705 - Natural Language Processing
-Homework 2 - Parsing with Context Free Grammars 
+Homework 2 - Parsing with Context Free Grammars
 Yassine Benajiba
 """
 import math
@@ -10,73 +10,87 @@ import itertools
 from grammar import Pcfg
 
 ### Use the following two functions to check the format of your data structures in part 3 ###
+
+
 def check_table_format(table):
     """
     Return true if the backpointer table object is formatted correctly.
-    Otherwise return False and print an error.  
+    Otherwise return False and print an error.
     """
-    if not isinstance(table, dict): 
+    if not isinstance(table, dict):
         sys.stderr.write("Backpointer table is not a dict.\n")
         return False
-    for split in table: 
-        if not isinstance(split, tuple) and len(split) ==2 and \
-          isinstance(split[0], int)  and isinstance(split[1], int):
-            sys.stderr.write("Keys of the backpointer table must be tuples (i,j) representing spans.\n")
+    for split in table:
+        if not isinstance(split, tuple) and len(split) == 2 and \
+                isinstance(split[0], int) and isinstance(split[1], int):
+            sys.stderr.write(
+                "Keys of the backpointer table must be tuples (i,j) representing spans.\n")
             return False
         if not isinstance(table[split], dict):
-            sys.stderr.write("Value of backpointer table (for each span) is not a dict.\n")
+            sys.stderr.write(
+                "Value of backpointer table (for each span) is not a dict.\n")
             return False
         for nt in table[split]:
-            if not isinstance(nt, str): 
-                sys.stderr.write("Keys of the inner dictionary (for each span) must be strings representing nonterminals.\n")
+            if not isinstance(nt, str):
+                sys.stderr.write(
+                    "Keys of the inner dictionary (for each span) must be strings representing nonterminals.\n")
                 return False
             bps = table[split][nt]
-            if isinstance(bps, str): # Leaf nodes may be strings
-                continue 
+            if isinstance(bps, str):  # Leaf nodes may be strings
+                continue
             if not isinstance(bps, tuple):
-                sys.stderr.write("Values of the inner dictionary (for each span and nonterminal) must be a pair ((i,k,A),(k,j,B)) of backpointers. Incorrect type: {}\n".format(bps))
+                sys.stderr.write(
+                    "Values of the inner dictionary (for each span and nonterminal) must be a pair ((i,k,A),(k,j,B)) of backpointers. Incorrect type: {}\n".format(bps))
                 return False
             if len(bps) != 2:
-                sys.stderr.write("Values of the inner dictionary (for each span and nonterminal) must be a pair ((i,k,A),(k,j,B)) of backpointers. Found more than two backpointers: {}\n".format(bps))
+                sys.stderr.write(
+                    "Values of the inner dictionary (for each span and nonterminal) must be a pair ((i,k,A),(k,j,B)) of backpointers. Found more than two backpointers: {}\n".format(bps))
                 return False
-            for bp in bps: 
-                if not isinstance(bp, tuple) or len(bp)!=3:
-                    sys.stderr.write("Values of the inner dictionary (for each span and nonterminal) must be a pair ((i,k,A),(k,j,B)) of backpointers. Backpointer has length != 3.\n".format(bp))
+            for bp in bps:
+                if not isinstance(bp, tuple) or len(bp) != 3:
+                    sys.stderr.write(
+                        "Values of the inner dictionary (for each span and nonterminal) must be a pair ((i,k,A),(k,j,B)) of backpointers. Backpointer has length != 3.\n".format(bp))
                     return False
                 if not (isinstance(bp[0], str) and isinstance(bp[1], int) and isinstance(bp[2], int)):
                     print(bp)
-                    sys.stderr.write("Values of the inner dictionary (for each span and nonterminal) must be a pair ((i,k,A),(k,j,B)) of backpointers. Backpointer has incorrect type.\n".format(bp))
+                    sys.stderr.write(
+                        "Values of the inner dictionary (for each span and nonterminal) must be a pair ((i,k,A),(k,j,B)) of backpointers. Backpointer has incorrect type.\n".format(bp))
                     return False
     return True
+
 
 def check_probs_format(table):
     """
     Return true if the probability table object is formatted correctly.
-    Otherwise return False and print an error.  
+    Otherwise return False and print an error.
     """
-    if not isinstance(table, dict): 
+    if not isinstance(table, dict):
         sys.stderr.write("Probability table is not a dict.\n")
         return False
-    for split in table: 
-        if not isinstance(split, tuple) and len(split) ==2 and isinstance(split[0], int) and isinstance(split[1], int):
-            sys.stderr.write("Keys of the probability must be tuples (i,j) representing spans.\n")
+    for split in table:
+        if not isinstance(split, tuple) and len(split) == 2 and isinstance(split[0], int) and isinstance(split[1], int):
+            sys.stderr.write(
+                "Keys of the probability must be tuples (i,j) representing spans.\n")
             return False
         if not isinstance(table[split], dict):
-            sys.stderr.write("Value of probability table (for each span) is not a dict.\n")
+            sys.stderr.write(
+                "Value of probability table (for each span) is not a dict.\n")
             return False
         for nt in table[split]:
-            if not isinstance(nt, str): 
-                sys.stderr.write("Keys of the inner dictionary (for each span) must be strings representing nonterminals.\n")
+            if not isinstance(nt, str):
+                sys.stderr.write(
+                    "Keys of the inner dictionary (for each span) must be strings representing nonterminals.\n")
                 return False
             prob = table[split][nt]
             if not isinstance(prob, float):
-                sys.stderr.write("Values of the inner dictionary (for each span and nonterminal) must be a float.{}\n".format(prob))
+                sys.stderr.write(
+                    "Values of the inner dictionary (for each span and nonterminal) must be a float.{}\n".format(prob))
                 return False
             if prob > 0:
-                sys.stderr.write("Log probability may not be > 0.  {}\n".format(prob))
+                sys.stderr.write(
+                    "Log probability may not be > 0.  {}\n".format(prob))
                 return False
     return True
-
 
 
 class CkyParser(object):
@@ -84,47 +98,74 @@ class CkyParser(object):
     A CKY parser.
     """
 
-    def __init__(self, grammar): 
+    def __init__(self, grammar):
         """
-        Initialize a new parser instance from a grammar. 
+        Initialize a new parser instance from a grammar.
         """
         self.grammar = grammar
 
-    def is_in_language(self,tokens):
+    def is_in_language(self, tokens):
         """
-        Membership checking. Parse the input tokens and return True if 
+        Membership checking. Parse the input tokens and return True if
         the sentence is in the language described by the grammar. Otherwise
         return False
+
         """
-        # TODO, part 2
-        return False 
-       
+        rules = list(self.grammar.rhs_to_rules)
+        pi_table = [[[] for i in range(len(tokens)+1)]
+                    for j in range(len(tokens)+1)]
+
+        for i in range(len(tokens)):
+            for tag, grammar_rules in self.grammar.rhs_to_rules.items():
+                if grammar_rules[0][1] == (tokens[i],):
+                    pi_table[i][i+1] = set()
+                    for item in self.grammar.rhs_to_rules[grammar_rules[0][1]]:
+                        pi_table[i][i+1].add(item[0])
+
+        for length in range(2, len(tokens) + 1):
+            for i in range(len(tokens) - length + 1):
+                j = i + length
+                for k in range(i + 1, j):
+                    if not pi_table[i][j]:
+                        pi_table[i][j] = set()
+                    for _, grammar_rules in self.grammar.rhs_to_rules.items():
+                        if len(grammar_rules[0][1]) == 2:
+                            for tag_1 in pi_table[i][k]:
+                                for tag_2 in pi_table[k][j]:
+                                    if tag_1 == grammar_rules[0][1][0] and tag_2 == grammar_rules[0][1][1]:
+                                        for item in self.grammar.rhs_to_rules[grammar_rules[0][1]]:
+                                            pi_table[i][j].add(item[0])
+
+        if pi_table[0][len(tokens) - 1]:
+            return True
+
+        return False
+
     def parse_with_backpointers(self, tokens):
         """
         Parse the input tokens and return a parse table and a probability table.
         """
         # TODO, part 3
-        table= None
+        table = None
         probs = None
         return table, probs
 
 
-def get_tree(chart, i,j,nt): 
+def get_tree(chart, i, j, nt):
     """
     Return the parse-tree rooted in non-terminal nt and covering span i,j.
     """
     # TODO: Part 4
-    return None 
- 
-       
+    return None
+
+
 if __name__ == "__main__":
-    
-    with open('atis3.pcfg','r') as grammar_file: 
-        grammar = Pcfg(grammar_file) 
+
+    with open('atis3.pcfg', 'r') as grammar_file:
+        grammar = Pcfg(grammar_file)
         parser = CkyParser(grammar)
-        toks =['flights', 'from','miami', 'to', 'cleveland','.'] 
-        #print(parser.is_in_language(toks))
-        #table,probs = parser.parse_with_backpointers(toks)
-        #assert check_table_format(chart)
-        #assert check_probs_format(probs)
-        
+        toks = ['flights', 'from', 'miami', 'to', 'cleveland', '.']
+        print(parser.is_in_language(toks))
+        # table,probs = parser.parse_with_backpointers(toks)
+        # assert check_table_format(chart)
+        # assert check_probs_format(probs)
--- hw2/grammar.py
+++ hw2/grammar.py
@@ -7,37 +7,38 @@ Yassine Benajiba
 import sys
 from collections import defaultdict
 from math import fsum
+import math
 
-class Pcfg(object): 
+
+class Pcfg(object):
     """
     Represent a probabilistic context free grammar. 
     """
 
-    def __init__(self, grammar_file): 
+    def __init__(self, grammar_file):
         self.rhs_to_rules = defaultdict(list)
         self.lhs_to_rules = defaultdict(list)
-        self.startsymbol = None 
-        self.read_rules(grammar_file)      
- 
-    def read_rules(self,grammar_file):
-        
-        for line in grammar_file: 
+        self.startsymbol = None
+        self.read_rules(grammar_file)
+
+    def read_rules(self, grammar_file):
+
+        for line in grammar_file:
             line = line.strip()
             if line and not line.startswith("#"):
-                if "->" in line: 
+                if "->" in line:
                     rule = self.parse_rule(line.strip())
                     lhs, rhs, prob = rule
                     self.rhs_to_rules[rhs].append(rule)
                     self.lhs_to_rules[lhs].append(rule)
-                else: 
+                else:
                     startsymbol, prob = line.rsplit(";")
                     self.startsymbol = startsymbol.strip()
-                    
-     
-    def parse_rule(self,rule_s):
+
+    def parse_rule(self, rule_s):
         lhs, other = rule_s.split("->")
         lhs = lhs.strip()
-        rhs_s, prob_s = other.rsplit(";",1) 
+        rhs_s, prob_s = other.rsplit(";", 1)
         prob = float(prob_s)
         rhs = tuple(rhs_s.strip().split())
         return (lhs, rhs, prob)
@@ -47,11 +48,37 @@ class Pcfg(object):
         Return True if the grammar is a valid PCFG in CNF.
         Otherwise return False. 
         """
-        # TODO, Part 1
-        return False 
+        for _, grammar_rules in self.lhs_to_rules.items():
+            for rule in grammar_rules:
+                rhs = rule[1]
+                if len(rhs) == 2:
+                    if rhs[0].upper() != rhs[0] and rhs[1].upper() != rhs[1]:
+                        print(
+                            f"The grammar can not be verified. {rhs[0]} and {rhs[1]} have to be both non terminal values")
+                        return False
+
+                elif len(rhs) == 1:
+                    if rhs[0].lower() != rhs[0]:
+                        print(
+                            f"The grammar can not be verified. {rhs[0]} has to be terminal value")
+                        return False
+                else:
+                    return False
+            prob_sum = fsum(rule[2] for rule in grammar_rules)
+            if abs(1 - prob_sum) > 0.00001:
+                print(prob_sum)
+                print(f"The probability of the same lhs symbol does not add up to 1")
+                return False
+        return True
 
 
 if __name__ == "__main__":
-    with open(sys.argv[1],'r') as grammar_file:
+
+    with open(sys.argv[1], 'r') as grammar_file:
         grammar = Pcfg(grammar_file)
-        
+
+        if grammar.verify_grammar():
+            print("Grammar is successfully verified. Grammar is a valid PCFG in CNF")
+        else:
+            print(
+                "Grammar is not successfully verified. Grammar is not a valid PCFG in CNF")
