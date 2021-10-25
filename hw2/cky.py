6c8cb4200adf3f4988d495ff49278c486ce21ecd
Davit Barblishvili <db3230@columbia.edu>
2021-10-24 21:49:33 -0400

    completed part 3

--- hw2/cky.py
+++ hw2/cky.py
@@ -146,8 +146,126 @@ class CkyParser(object):
         Parse the input tokens and return a parse table and a probability table.
         """
         # TODO, part 3
-        table = None
-        probs = None
+        table = dict()
+        probs = dict()
+        len_tokens = len(tokens)
+        # table dict initialization
+        for i in range(len_tokens+1):
+            for j in range(len_tokens+1):
+                table[(i, j)] = dict()
+
+        # prob dict initialization
+        for i in range(len_tokens+1):
+            for j in range(len_tokens+1):
+                probs[(i, j)] = dict()
+
+        for i in range(len_tokens):
+            for _, grammar_rules in self.grammar.rhs_to_rules.items():
+                if grammar_rules[0][1] == (tokens[i],):
+                    table[(i, i+1)] = dict()
+                    probs[(i, i+1)] = dict()
+                    for item in self.grammar.rhs_to_rules[grammar_rules[0][1]]:
+                        table[(i, i+1)][item[0]] = item[1][0]
+                        probs[(i, i+1)][item[0]] = math.log(item[2])
+
+        for length in range(2, len_tokens + 1):
+            for i in range(len_tokens - length + 1):
+                j = i + length
+                for k in range(i + 1, j):
+                    if (i, j) not in table.keys():
+                        table[(i, j)] = dict()
+                        probs[(i, j)] = dict()
+                for _, grammar_rules in self.grammar.rhs_to_rules.items():
+                    if len(grammar_rules[0][1]) == 2:
+                        for tag_1 in table[(i, k)].keys():
+                            for tag_2 in table[(k, j)].keys():
+                                if tag_1 == grammar_rules[0][1][0] and tag_2 == grammar_rules[0][1][1]:
+                                    for item in self.grammar.rhs_to_rules[grammar_rules[0][1]]:
+                                        if item[2] != 0:
+                                            items = ((tag_1, i, k),
+                                                     (tag_2, k, j))
+                                            prob = math.log(
+                                                item[2]) + probs[(i, k)][tag_1] + probs[(k, j)][tag_2]
+                                            if item[0] not in probs[(i, j)]:
+                                                probs[(i, j)][item[0]] = prob
+                                                table[(i, j)][item[0]] = items
+                                            else:
+                                                current_prob = probs[(
+                                                    i, j)][item[0]]
+                                                if prob > current_prob:
+                                                    probs[(i, j)][item[0]
+                                                                  ] = prob
+                                                    table[(i, j)][item[0]
+                                                                  ] = items
+                                        else:
+                                            current_prob = 0
+                                            if item[0] not in table[(i, j)]:
+                                                table[(i, j)][item[0]] = (
+                                                    (tag_1, i, k), (tag_2, k, j))
+                                                probs[(i, j)][item[0]
+                                                              ] = current_prob
+        return table, probs
+
+    def parse_with_backpointers1(self, tokens):
+
+        rules = list(self.grammar.rhs_to_rules)
+        n = len(tokens)
+        table = dict()
+        probs = dict()
+        for i in range(n+1):
+            for j in range(n+1):
+                table[(i, j)] = dict()
+        for i in range(n+1):
+            for j in range(n+1):
+                probs[(i, j)] = dict()
+
+        for i in range(n):
+            for rule in rules:
+                if rule == (tokens[i],):
+                    table[(i, i+1)] = dict()
+                    probs[(i, i+1)] = dict()
+                    for x in self.grammar.rhs_to_rules[rule]:
+                        table[(i, i+1)][x[0]] = x[1][0]
+                        probs[(i, i+1)][x[0]] = math.log(x[2])
+        for length in range(2, n+1):
+            for i in range(n-length+1):
+                j = i+length
+                for k in range(i+1, j):
+                    if (i, j) not in table.keys():
+                        table[(i, j)] = dict()
+                        probs[(i, j)] = dict()
+                    for rule in rules:
+                        if len(rule) == 2:
+                            #print(rule, table[(i, k)],table[(k,j)])
+                            for symbol1 in table[(i, k)].keys():
+                                for symbol2 in table[(k, j)].keys():
+                                    if symbol1 == rule[0] and symbol2 == rule[1]:
+                                        for x in self.grammar.rhs_to_rules[rule]:
+                                            if not x[2] == 0:
+                                                children = (
+                                                    (symbol1, i, k), (symbol2, k, j))
+                                                probabiity = math.log(
+                                                    x[2]) + probs[(i, k)][symbol1] + probs[(k, j)][symbol2]
+                                                if x[0] not in probs[(i, j)]:
+                                                    probs[(i, j)][x[0]
+                                                                  ] = probabiity
+                                                    table[(i, j)][x[0]
+                                                                  ] = children
+                                                else:
+                                                    current_prob = probs[(
+                                                        i, j)][x[0]]
+                                                    if probabiity > current_prob:
+                                                        probs[(i, j)][x[0]
+                                                                      ] = probabiity
+                                                        table[(i, j)][x[0]
+                                                                      ] = children
+                                            else:
+                                                current_prob = 0
+                                                if x[0] not in table[(i, j)]:
+                                                    table[(i, j)][x[0]] = (
+                                                        (symbol1, i, k), (symbol2, k, j))
+                                                    probs[(i, j)][x[0]
+                                                                  ] = current_prob
         return table, probs
 
 
@@ -166,6 +284,7 @@ if __name__ == "__main__":
         parser = CkyParser(grammar)
         toks = ['flights', 'from', 'miami', 'to', 'cleveland', '.']
         print(parser.is_in_language(toks))
-        # table,probs = parser.parse_with_backpointers(toks)
-        # assert check_table_format(chart)
-        # assert check_probs_format(probs)
+        table, probs = parser.parse_with_backpointers(toks)
+
+        assert check_table_format(table)
+        assert check_probs_format(probs)
